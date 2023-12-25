69b0e66669caca0c708c8ee261a31b3248484a45
kirika <2072474790@qq.com>
2023-12-25 20:54:58 +0800

    完成了购物车相关业务代码开发.

--- sky-pojo/src/main/java/com/sky/dto/ShoppingCartDTO.java
+++ sky-pojo/src/main/java/com/sky/dto/ShoppingCartDTO.java
@@ -1,7 +1,9 @@
 package com.sky.dto;
 
+import com.sky.entity.ShoppingCart;
 import lombok.Data;
 import java.io.Serializable;
+import java.util.List;
 
 @Data
 public class ShoppingCartDTO implements Serializable {
